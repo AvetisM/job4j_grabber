67a98512f4d8b6cb1832ac3cea0c896b74283ba4
AvetisM <avetis.mkhitaryants@gmail.com>
2022-03-19 13:18:23 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class HabrCareerParse {
@@ -29,9 +30,28 @@ public class HabrCareerParse {
                 Element dateElement = row.select(".vacancy-card__date").first().child(0);
                 String dateAttribute = dateElement.attr("datetime");
                 LocalDateTime date = new HabrCareerDateTimeParser().parse(dateAttribute);
-                System.out.printf("%s %s %s%n", vacancyName, link, date);
+                String description = retrieveDescription(link);
+                Post post = new Post(vacancyName, link, description, date);
+                System.out.println(post.toString());
             });
         }
+    }
 
+    private static String retrieveDescription(String link) {
+        StringBuffer stringBuffer = new StringBuffer();
+        Connection connection = Jsoup.connect(link);
+        try {
+            Document document = connection.get();
+            Elements rows = document.select(".job_show_description__vacancy_description");
+            rows.forEach(row -> {
+                Element descriptionElement = row.child(0);
+                stringBuffer
+                        .append(descriptionElement.text())
+                        .append(System.lineSeparator());
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
     }
 }
--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -19,7 +19,6 @@ public class Post {
     }
 
     public Post(String title, String link, String description, LocalDateTime created) {
-        this.id = id;
         this.title = title;
         this.link = link;
         this.description = description;
