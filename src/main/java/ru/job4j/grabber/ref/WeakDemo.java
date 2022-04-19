765536e24e872c6e23bef7b972ca3e6deb0f635f
AvetisM <avetis.mkhitaryants@gmail.com>
2022-04-19 22:18:48 +0300

    0. Виды ссылок [#6854]

--- src/main/java/ru/job4j/grabber/ref/WeakDemo.java
+++ src/main/java/ru/job4j/grabber/ref/WeakDemo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class WeakDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        //example1();
+        example1();
         example2();
     }
 
